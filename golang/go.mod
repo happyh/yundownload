4dce825c0b281b71fd6e18934c502a4870a198fc
root <root@192.168.10.196>
2024-06-28 22:54:49 +0800

    修复screen启动没有传递p参数的问题

--- golang/go.mod
+++ golang/go.mod
@@ -1,4 +1,4 @@
-module aliyundownload
+module download
 
 go 1.17
 
--- golang/main.go
+++ golang/main.go
@@ -55,7 +55,7 @@ func main() {
 				fmt.Printf("当前目录：%s, 下载文件：%s 已经后台screen执行，可screen -r进入查看下载进度\n", pwd, ef2File)
 
 				// 构造要执行的命令
-				command := fmt.Sprintf("%s %s --noscreen", os.Args[0], ef2File)
+				command := fmt.Sprintf("%s %s --noscreen -p %d", os.Args[0], ef2File,parallel)
 
 				// 使用screen执行命令
 				cmd := exec.Command("screen", "-dmS", "my_screen", "bash", "-c", command)
